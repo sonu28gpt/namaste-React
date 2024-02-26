5710b55ec33c7f38200b991823fa296cf0b8986f
sonu28gpt <sonu28gpt@gmail.com>
2024-02-26 12:43:30 +0530

    lecture2 ka kaam

--- lecture1/App.js
+++ lecture1/App.js
@@ -1,23 +1,11 @@
-/*
-<div id=parent>
-    div id=child
-        h1
-        h2
-    div
-    div id=child2
-        h1
-        h2
-    div
-div
-
-
-*/
-
+import React from "react";
+import  ReactDOM  from "react-dom";
 
 const heading=React.createElement('h1',{id:'heading'},'react hai apun');
 console.log(heading);//object i.e. react element
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
+
 
 const parent=React.createElement('div',{id:"parent"},[
     React.createElement('div',{id:'child1'},[
